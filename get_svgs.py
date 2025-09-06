a51f80a72c9bd7fc7bebd4b39dd2ac30e1dedfb2
tuzicao <13718054285@163.com>
2025-09-06 18:48:24 +0800

    增加预览html

--- get_svgs.py
+++ get_svgs.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 template = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -64 1024 1000" style="transform: scaleY(-1);">
@@ -7,12 +8,28 @@ template = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -64 1024 1000" 
 with open("rpgawesome-webfont.svg", "r", encoding="utf-8") as f:
     content = f.read()
 
+res = []
+
 for line in content.split("\n"):
     if "glyph-name" in line:
         name = re.search(r'glyph-name="(.+?)"', line).group(1)
         print(name)
+        res.append({"name": name, "path": f"rpg-awesome-svg/{name}.svg"})
         pathdata = re.search(r' d="(.+?)"', line).group(1)
         with open(f"rpg-awesome-svg/{name}.svg", "w", encoding="utf-8") as f:
             f.write(template.format(pathData=pathdata))
         # break
 
+with open("preview.js", "w", encoding="utf-8") as f:
+    f.write(
+        f"""const svg_list={json.dumps(res)}"""
+        + """
+let sample = document.getElementById("sample")
+svg_list.map((i) =>
+  sample.insertAdjacentHTML(
+    "beforeend",
+    `<div class="svg-item"><img src="./${i.path}"/><div>${i.name}</div></div>`
+  )
+)
+"""
+    )
